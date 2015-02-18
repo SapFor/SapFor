8e0941a07e147fba415a50d4e32386b47e3f93af
CChabert <CChabert@users.noreply.github.com>
2015-02-18 16:07:29 +0100

    Create SapForController.java

---
+++ ClientIHM/SapForController.java
@@ -0,0 +1,21 @@
+/* SapFor Project 2015
+ * 
+ * Author : Caroline Chabert
+ */
+
+package controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class SapForController implements Initializable {
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		
+		
+	}
+
+}
